cc9209944501ee9a94414b721b753a05cd7c00a8
Elizabeth Greene <egreene@microsoft.com>
2025-06-19 15:40:44 -0500

    bump version number for next release

--- version.h
+++ version.h
@@ -1,7 +1,7 @@
 #define VERSION_MAJOR 1
 #define VERSION_MINOR 2
 #define VERSION_BUILD 0
-#define VERSION_REVISION 3
+#define VERSION_REVISION 4
 
 // Helper macro for stringification  
 #define STRINGIFY(x) #x  
